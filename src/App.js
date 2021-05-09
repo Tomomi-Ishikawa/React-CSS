3ccdaf6d10f1d366720c8936a6b82ef5eb9e6c07
Tomomi-Ishikawa <68460000+Tomomi-Ishikawa@users.noreply.github.com>
2021-05-09 17:38:00 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import { InlineStyle } from "./components/inlineStyle";
+
+export default function App() {
+  return (
+    <div className="App">
+      <InlineStyle />
+    </div>
+  );
+}
---
+++ src/components/inlineStyle.jsx
@@ -0,0 +1,28 @@
+export const InlineStyle = () => {
+  // CSS 値は文字列記述　　接続詞は大文字
+  const containnerStyle = {
+    border: "solid 2px #392eff",
+    borderRadius: "20px",
+    padding: "8px",
+    margin: "8px",
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center"
+  };
+  const titleStyle = {
+    margin: 0,
+    color: "#3db84a8"
+  };
+  const buttonStyle = {
+    backgroundColor: " #abedd8",
+    border: "none",
+    padding: "8px",
+    borderRadius: "8px"
+  };
+  return (
+    <div style={containnerStyle}>
+      <p style={titleStyle}>-InlineStyle-</p>
+      <button style={buttonStyle}>FIGHT</button>
+    </div>
+  );
+};
